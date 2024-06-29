f71b631bc9c434b7d48f05a18556f3acb6bdfd92
JuneBridge <junebridge03@g.skku.edu>
2024-06-29 20:50:33 +0900

    Time: 51 ms (92.34%), Space: 17.9 MB (7.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,24 @@
+import collections
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numCnt = collections.defaultdict(int)
+
+        minNum = None
+
+        for e in list(set(nums)):
+            minimum = min(e, target - e)
+            numCnt[minimum] += 1
+            if numCnt[minimum] == 2:
+                minNum = minimum
+                break
         
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        
-        
+        if minNum == None:
+            minNum = target // 2
+
+        if minNum * 2 != target:
+            return [nums.index(minNum), nums.index(target - minNum)]
+        else:
+            minIndex = nums.index(minNum)
+            return [minIndex, nums[minIndex + 1:].index(minNum) + minIndex + 1]
+
