04622eaf17d429ad04bb42829311dc9356c9cc3f
JuneBridge <junebridge03@g.skku.edu>
2024-06-30 15:46:49 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 528 ms, Memory: 69684 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,19 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+DIVISOR = 1000000007
+
 N = int(input())
+arr = list(map(int, input().split()))
+
 
 if N == 1:
     print(0)
     exit(0)
 
-arr = list(map(int, input().split()))
+if N == 2:
+    print(abs(arr[1] - arr[0]) % DIVISOR)
+    exit(0)
 
 arr.sort()
 
-if N == 2:
-    print((arr[1] - arr[0]) % 1000000007)
-    exit(0)
+twoMuls = []
+e = 2
+for _ in range(N - 1):
+    twoMuls.append(e - 1)
+    e = (e * 2) % DIVISOR
+
 
 s = 0
-for i in range(len(arr) - 1):
-    s += (arr[i + 1] - arr[i]) * (2 ** (i + 1) - 1) * (2 ** (N - i - 1) - 1)
+for i in range(N - 1):
+    s += ((arr[i + 1] - arr[i]) * twoMuls[i] * twoMuls[N - 2 - i]) % DIVISOR
 
-print(s % 1000000007)
+print(s % DIVISOR)
