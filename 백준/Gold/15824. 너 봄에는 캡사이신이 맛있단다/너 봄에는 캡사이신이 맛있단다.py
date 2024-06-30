01e2638f0681c5e96959dec84c3913b916b12b2e
JuneBridge <junebridge03@g.skku.edu>
2024-06-30 15:10:09 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 132 ms, Memory: 110536 KB, Score: 50 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,3 +1,5 @@
+DIVISOR = 1000000007
+
 N = int(input())
 
 if N == 1:
@@ -9,11 +11,12 @@ arr = list(map(int, input().split()))
 arr.sort()
 
 if N == 2:
-    print((arr[1] - arr[0]) % 1000000007)
+    print((arr[1] - arr[0]) % DIVISOR)
     exit(0)
 
 s = 0
 for i in range(len(arr) - 1):
-    s += (arr[i + 1] - arr[i]) * (2 ** (i + 1) - 1) * (2 ** (N - i - 1) - 1)
+    s += ((arr[i + 1] - arr[i]) * (2 ** (i + 1) - 1) * (2 ** (N - i - 1) - 1)) % DIVISOR
+    s = s % DIVISOR
 
-print(s % 1000000007)
+print(s % DIVISOR)
