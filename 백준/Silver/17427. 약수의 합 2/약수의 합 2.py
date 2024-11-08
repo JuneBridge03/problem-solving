7568e075d593112e8a0f9e90d04243735e6c6aff
JuneBridge <junebridge03@g.skku.edu>
2024-11-08 16:21:18 +0900

    [Silver II] Title: 약수의 합 2, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
@@ -1,3 +1,11 @@
 N = int(input())
 
-print(sum([(N // i) * i for i in range(1, N + 1)]))
+answer = 0
+for i in range(1, N + 1):
+    now = (N // i) * i
+    answer += now
+    if now == 1:
+        answer += (N - i)
+        break
+
+print(answer)
